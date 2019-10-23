b70c90f74013e81450813f018ebe650a4c7449c7
samar-khaled <samar.khaled@gmail.com>
2019-10-23 14:07:31 +0200

    Create UIColor easily with RGB and HEX and generate random color for testing

---
+++ UIColorExtension.swift
@@ -0,0 +1,54 @@
+//
+//  UIColorExtension.swift
+//
+//  Created by Smsma Mac on 9/8/19.
+//  Copyright © 2019 Samar Khaled. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    // Create a UIColor from RGB
+    convenience init(red: Int, green: Int, blue: Int, a: CGFloat = 1.0) {
+        
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: a
+        )
+    }
+    
+    // Create a UIColor from a hex value (E.g 0x000000)
+    convenience init(hex: Int, a: CGFloat = 1.0) {
+        self.init(
+            red: (hex >> 16) & 0xFF,
+            green: (hex >> 8) & 0xFF,
+            blue: hex & 0xFF,
+            a: a
+        )
+    }
+    
+    // Get RGBa from UIColor.
+    public var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        return (red, green, blue, alpha)
+    }
+    
+    // get random color, used for testing..
+    class var random: UIColor {
+        return UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
+    }
+    
+}
