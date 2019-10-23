5f2d77e9e9cbc0ba29d9dae58d39b8a269ce42f1
samar-khaled <samar.khaled@gmail.com>
2019-10-23 14:09:01 +0200

    access UIView frame properties easily

---
+++ UIView+Frame.swift
@@ -0,0 +1,43 @@
+//
+//  UIView+Frame.swift
+//
+//  Created by Smsma Mac on 9/8/19.
+//  Copyright © 2019 Samar Khaled. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        get { return self.frame.size.width }
+        set { self.frame.size.width = newValue }
+    }
+    
+    public var height: CGFloat {
+        get { return self.frame.size.height }
+        set { self.frame.size.height = newValue }
+    }
+    
+    public var left: CGFloat {
+        get { return self.frame.origin.x }
+        set { self.frame.origin.x = newValue }
+    }
+    
+    public var top: CGFloat {
+        get { return self.frame.origin.y }
+        set { self.frame.origin.y = newValue }
+    }
+    
+    public var right: CGFloat {
+        get { return self.frame.origin.x + self.width }
+        set { self.frame.origin.x = newValue - self.width }
+    }
+    
+    public var bottom: CGFloat {
+        get { return self.frame.origin.y + self.height }
+        set { self.frame.origin.y = newValue - self.height }
+    }
+    
+}
